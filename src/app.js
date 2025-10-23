910408acc325f7a1823fe5fdf2b24a3e1909f712
LuisoDev1 <luisbet92@gmail.com>
2025-10-23 05:39:38 +0000

    agregando estilos

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@ import "bootstrap";
 import "./style.css";
 
 
+
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const quien = ["Mi perro", "Mi abuela", "Mi Primo", "Mi tia", "mi amigo","mi gato"];
+  const accion = ["se comio mi tarea", "se fue de fiesta", "se hizo el delicisoso", "se puso a llorar","se volvio loco", "araño a un perro"];
+  
+  let quienAleatorio = generadorDeExcusas(quien);
+  let accionAleatorio = generadorDeExcusas(accion);
+
+  let elemento = document.getElementById("excuse");
+  elemento.innerHTML = `${quienAleatorio} ${accionAleatorio}`;
+
+  /*console.log(elemento.innerHTML);*/
 };
+
+function generadorDeExcusas(lista) {
+  let indiceAleatorio = Math.floor(Math.random() * lista.length);
+  return lista[indiceAleatorio];
+}
