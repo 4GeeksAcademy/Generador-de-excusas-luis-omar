a40faa1182a456ee8d41e9a071e24d1ce6bea531
LuisoDev1 <luisbet92@gmail.com>
2025-11-18 00:47:02 +0000

    agregando los 2 arrays faltantes

--- src/app.js
+++ src/app.js
@@ -8,38 +8,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-const quien = [
-  "Mi guitarra celosa",
-  "El pianista del grupo",
-  "Mi profe de solfeo",
-  "El bajista dormilón",
-  "El baterista filosófico",
-  "Mi micro que siempre se apaga",
-  "El director de la banda",
-  "El DJ del barrio",
-  "Mi afinador con actitud",
-  "El trompetista romántico"
-];
 
-const accion = [
-  "no me dejó salir hasta que afiné hasta el alma.",
-  "dijo que hoy no podía practicar porque el ritmo no estaba de humor.",
-  "me pidió que tocara un bolero pa’ ver si volvía su ex.",
-  "me hizo repetir la canción porque dijo que el do sonó con pereza.",
-  "me juró que los acordes menores traen mala suerte los lunes.",
-  "se enojó porque bailé fuera del tiempo y me castigó con metrónomo.",
-  "me convenció de que el silencio también cuenta como ensayo.",
-  "se deprimió porque el WiFi entró en modo bemol.",
-  "me hizo acompañarlo a buscar inspiración al puesto de fritas.",
-  "dijo que sin café no hay compás ni esperanza."
-];
+let quien = ['El baterista', 'Mi compa de la banda', 'El chavo del sonido', 'El DJ del barrio'];
+let accion = ['se puso creativo y movió', 'se tropezó y tiró', 'apachurró sin querer', 'se emocionó y desconectó'];
+let que = ['mi guitarra', 'el micro', 'los parlantes', 'la mezcladora', 'el cable principal que nadie debe tocar'];
+let cuando = ['antes del toque', 'justo cuando afinaba', 'en pleno jamming', 'cuando tomaba agua', 'dos segundos antes de empezar la rola'];
 
   
   //let quienAleatorio = generadorDeExcusas(quien);
   //let accionAleatorio = generadorDeExcusas(accion);
 
+  let excusaCompleta = `${generadorDeExcusas(quien)} ${generadorDeExcusas(accion)} ${generadorDeExcusas(que)} ${generadorDeExcusas(cuando)}`;
   let elemento = document.getElementById("excuse");
-  elemento.innerHTML = `${generadorDeExcusas(quien)} ${generadorDeExcusas(accion)}`;
+  elemento.innerHTML = excusaCompleta;
 
   /*console.log(elemento.innerHTML);*/
 };
